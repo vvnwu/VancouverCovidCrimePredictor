794f3ef8df9b31ea1450bc36db8b1bd0d300bb8b
Vivian Wu <wu.vivian00@gmail.com>
2020-12-07 01:52:56 -0800

    changed dates to datetime objects

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# load data
+census = pd.read_csv("data/Raw Data/98-400-X2016114_English_CSV_data.csv")
+crime_raw = pd.read_csv("data/Raw Data/crimedata_csv_all_years.csv")
+mobility = pd.read_csv("data/Raw Data/2020_CA_Region_Mobility_Report.csv")
+# PREPROCESSING
+# aggregate time data
+mobility['date'] = pd.to_datetime(mobility['date'])
+# mobility = df.
+
+crime_raw['YEAR'] = crime_raw['YEAR'].astype(str)
+crime_raw['MONTH'] = crime_raw['MONTH'].astype(str)
+crime_raw['DAY'] = crime_raw['DAY'].astype(str)
+crime_raw['date'] = crime_raw['YEAR'] + '-' + crime_raw['MONTH'] + '-' + crime_raw['DAY']
+crime_raw['date'] = pd.to_datetime(crime_raw['date'])
+
+crime = crime_raw[['']]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
